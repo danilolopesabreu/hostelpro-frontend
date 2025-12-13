9ab6c7f3c032525ff1a88c3e2782df8c56ae7e16
Danilo Abreu <danilo.abreu@redemobconsorcio.com.br>
2025-12-12 23:43:25 -0300

    correção url base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://personal-deer-agiliza-9757f706.koyeb.app/', // ambiente de produção
+  apiUrl: 'https://personal-deer-agiliza-9757f706.koyeb.app/api', // ambiente de produção
   auth0: {
     domain: 'dev-75u3tgtrzvndx1o0.us.auth0.com',
     clientId: 'bG0qIsZx3GkCVF5RWJosI9TkEW8caqOI',
